712ef08d09505edd157b5890cd294f9b95e2fcee
diversen <dennis.iversen@gmail.com>
2011-03-07 15:36:44 +0100

    moved identicon and made it a class.

--- lib/identicon.php
+++ lib/identicon.php
@@ -1,8 +1,18 @@
 <?php
 
+// sligtly modified from: 
+// http://sourceforge.net/users/bcosca
+// Project: 
+//http://sourceforge.net/projects/identicons/
+
 /* generate sprite for corners and sides */
+
+class identicon {
+
+    public static $spriteZ = null;
+
 function getsprite($shape,$R,$G,$B,$rotation) {
-	global $spriteZ;
+	$spriteZ = self::$spriteZ;
 	$sprite=imagecreatetruecolor($spriteZ,$spriteZ);
 	//imageantialias($sprite,TRUE);
 	$fg=imagecolorallocate($sprite,$R,$G,$B);
@@ -188,7 +198,7 @@ function getsprite($shape,$R,$G,$B,$rotation) {
 
 /* generate sprite for center block */
 function getcenter($shape,$fR,$fG,$fB,$bR,$bG,$bB,$usebg) {
-	global $spriteZ;
+	$spriteZ = self::$spriteZ;
 	$sprite=imagecreatetruecolor($spriteZ,$spriteZ);
 	//imageantialias($sprite,TRUE);
 	$fg=imagecolorallocate($sprite,$fR,$fG,$fB);
@@ -301,84 +311,89 @@ function getcenter($shape,$fR,$fG,$fB,$bR,$bG,$bB,$usebg) {
 }
 
 /* parse hash string */
+function save_identicon ($hash, $size, $filename) {
+    $csh=hexdec(substr($hash,0,1)); // corner sprite shape
+    $ssh=hexdec(substr($hash,1,1)); // side sprite shape
+    $xsh=hexdec(substr($hash,2,1))&7; // center sprite shape
 
-$csh=hexdec(substr($_GET["hash"],0,1)); // corner sprite shape
-$ssh=hexdec(substr($_GET["hash"],1,1)); // side sprite shape
-$xsh=hexdec(substr($_GET["hash"],2,1))&7; // center sprite shape
+    $cro=hexdec(substr($hash,3,1))&3; // corner sprite rotation
+    $sro=hexdec(substr($hash,4,1))&3; // side sprite rotation
+    $xbg=hexdec(substr($hash,5,1))%2; // center sprite background
 
-$cro=hexdec(substr($_GET["hash"],3,1))&3; // corner sprite rotation
-$sro=hexdec(substr($_GET["hash"],4,1))&3; // side sprite rotation
-$xbg=hexdec(substr($_GET["hash"],5,1))%2; // center sprite background
+    /* corner sprite foreground color */
+    $cfr=hexdec(substr($hash,6,2));
+    $cfg=hexdec(substr($hash,8,2));
+    $cfb=hexdec(substr($hash,10,2));
 
-/* corner sprite foreground color */
-$cfr=hexdec(substr($_GET["hash"],6,2));
-$cfg=hexdec(substr($_GET["hash"],8,2));
-$cfb=hexdec(substr($_GET["hash"],10,2));
+    /* side sprite foreground color */
+    $sfr=hexdec(substr($hash,12,2));
+    $sfg=hexdec(substr($hash,14,2));
+    $sfb=hexdec(substr($hash,16,2));
 
-/* side sprite foreground color */
-$sfr=hexdec(substr($_GET["hash"],12,2));
-$sfg=hexdec(substr($_GET["hash"],14,2));
-$sfb=hexdec(substr($_GET["hash"],16,2));
+    /* final angle of rotation */
+    $angle=hexdec(substr($hash,18,2));
 
-/* final angle of rotation */
-$angle=hexdec(substr($_GET["hash"],18,2));
+    /* size of each sprite */
+    $spriteZ= self::$spriteZ = 128;
+    //$spriteZ=128;
+
+    /* start with blank 3x3 identicon */
+    $identicon=imagecreatetruecolor($spriteZ*3,$spriteZ*3);
+    //imageantialias($identicon,TRUE);
+
+    /* assign white as background */
+    $bg=imagecolorallocate($identicon,255,255,255);
+    imagefilledrectangle($identicon,0,0,$spriteZ,$spriteZ,$bg);
+
+    /* generate corner sprites */
+    $corner=self::getsprite($csh,$cfr,$cfg,$cfb,$cro);
+    imagecopy($identicon,$corner,0,0,0,0,$spriteZ,$spriteZ);
+    $corner=imagerotate($corner,90,$bg);
+    imagecopy($identicon,$corner,0,$spriteZ*2,0,0,$spriteZ,$spriteZ);
+    $corner=imagerotate($corner,90,$bg);
+    imagecopy($identicon,$corner,$spriteZ*2,$spriteZ*2,0,0,$spriteZ,$spriteZ);
+    $corner=imagerotate($corner,90,$bg);
+    imagecopy($identicon,$corner,$spriteZ*2,0,0,0,$spriteZ,$spriteZ);
+
+    /* generate side sprites */
+    $side=self::getsprite($ssh,$sfr,$sfg,$sfb,$sro);
+    imagecopy($identicon,$side,$spriteZ,0,0,0,$spriteZ,$spriteZ);
+    $side=imagerotate($side,90,$bg);
+    imagecopy($identicon,$side,0,$spriteZ,0,0,$spriteZ,$spriteZ);
+    $side=imagerotate($side,90,$bg);
+    imagecopy($identicon,$side,$spriteZ,$spriteZ*2,0,0,$spriteZ,$spriteZ);
+    $side=imagerotate($side,90,$bg);
+    imagecopy($identicon,$side,$spriteZ*2,$spriteZ,0,0,$spriteZ,$spriteZ);
+
+    /* generate center sprite */
+    $center=self::getcenter($xsh,$cfr,$cfg,$cfb,$sfr,$sfg,$sfb,$xbg);
+    imagecopy($identicon,$center,$spriteZ,$spriteZ,0,0,$spriteZ,$spriteZ);
+
+    // $identicon=imagerotate($identicon,$angle,$bg);
+
+    /* make white transparent */
+    imagecolortransparent($identicon,$bg);
+
+    /* create blank image according to specified dimensions */
+    $resized=imagecreatetruecolor($size,$size);
+    //imageantialias($resized,TRUE);
+
+    /* assign white as background */
+    $bg=imagecolorallocate($resized,255,255,255);
+    imagefilledrectangle($resized,0,0,$size,$size,$bg);
+
+    /* resize identicon according to specification */
+    imagecopyresampled($resized,$identicon,0,0,(imagesx($identicon)-$spriteZ*3)/2,(imagesx($identicon)-$spriteZ*3)/2,$size,$size,$spriteZ*3,$spriteZ*3);
+
+    /* make white transparent */
+    imagecolortransparent($resized,$bg);
+
+    /* and finally, send to standard output */
+    //header("Content-Type: image/png");
+    imagepng($resized, $filename, 0, null);
+}
 
 /* size of each sprite */
-$spriteZ=128;
-
-/* start with blank 3x3 identicon */
-$identicon=imagecreatetruecolor($spriteZ*3,$spriteZ*3);
-//imageantialias($identicon,TRUE);
-
-/* assign white as background */
-$bg=imagecolorallocate($identicon,255,255,255);
-imagefilledrectangle($identicon,0,0,$spriteZ,$spriteZ,$bg);
-
-/* generate corner sprites */
-$corner=getsprite($csh,$cfr,$cfg,$cfb,$cro);
-imagecopy($identicon,$corner,0,0,0,0,$spriteZ,$spriteZ);
-$corner=imagerotate($corner,90,$bg);
-imagecopy($identicon,$corner,0,$spriteZ*2,0,0,$spriteZ,$spriteZ);
-$corner=imagerotate($corner,90,$bg);
-imagecopy($identicon,$corner,$spriteZ*2,$spriteZ*2,0,0,$spriteZ,$spriteZ);
-$corner=imagerotate($corner,90,$bg);
-imagecopy($identicon,$corner,$spriteZ*2,0,0,0,$spriteZ,$spriteZ);
-
-/* generate side sprites */
-$side=getsprite($ssh,$sfr,$sfg,$sfb,$sro);
-imagecopy($identicon,$side,$spriteZ,0,0,0,$spriteZ,$spriteZ);
-$side=imagerotate($side,90,$bg);
-imagecopy($identicon,$side,0,$spriteZ,0,0,$spriteZ,$spriteZ);
-$side=imagerotate($side,90,$bg);
-imagecopy($identicon,$side,$spriteZ,$spriteZ*2,0,0,$spriteZ,$spriteZ);
-$side=imagerotate($side,90,$bg);
-imagecopy($identicon,$side,$spriteZ*2,$spriteZ,0,0,$spriteZ,$spriteZ);
-
-/* generate center sprite */
-$center=getcenter($xsh,$cfr,$cfg,$cfb,$sfr,$sfg,$sfb,$xbg);
-imagecopy($identicon,$center,$spriteZ,$spriteZ,0,0,$spriteZ,$spriteZ);
-
-// $identicon=imagerotate($identicon,$angle,$bg);
-
-/* make white transparent */
-imagecolortransparent($identicon,$bg);
-
-/* create blank image according to specified dimensions */
-$resized=imagecreatetruecolor($_GET["size"],$_GET["size"]);
-//imageantialias($resized,TRUE);
-
-/* assign white as background */
-$bg=imagecolorallocate($resized,255,255,255);
-imagefilledrectangle($resized,0,0,$_GET["size"],$_GET["size"],$bg);
-
-/* resize identicon according to specification */
-imagecopyresampled($resized,$identicon,0,0,(imagesx($identicon)-$spriteZ*3)/2,(imagesx($identicon)-$spriteZ*3)/2,$_GET["size"],$_GET["size"],$spriteZ*3,$spriteZ*3);
-
-/* make white transparent */
-imagecolortransparent($resized,$bg);
-
-/* and finally, send to standard output */
-header("Content-Type: image/png");
-imagepng($resized);
-
+//$spriteZ=128;
+}
 ?>
