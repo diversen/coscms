70ff146bdadfb76a063dc2879920dfc444bed3b4
diversen <dennis.iversen@gmail.com>
2011-03-12 17:18:44 +0100

    added a much simplified form class. will html to this class.

---
+++ lib/html.php
@@ -0,0 +1,111 @@
+<?php
+
+
+class HTML {
+
+    public static $values = array();
+    public static $formStr = '';
+    public static $autoLoadTrigger;
+
+    public static function init ($values = array ()) {
+
+        if (!empty(self::$autoLoadTrigger)){
+            $trigger = self::$autoLoadTrigger;
+            if (isset($_POST[$trigger])) {
+                self::$values = $_POST;
+            } else if (isset($_GET[$trigger])){
+                self::$values = $_GET;
+            } else {
+                self::$values = $values;
+            }
+        } 
+    }
+
+    public static function setValues ($values) {
+        self::$values = $values;
+    }
+
+    public static function formStart (
+            $name = 'form', $method ='post', $action = '',
+            $enctype = "multipart/form-data") {
+        $str = "<form action=\"$action\" method=\"$method\" name=\"$name\" $enctype = \"$enctype\">\n";
+        $str.= "<fieldset>\n";
+        self::$formStr.= $str;
+        return $str;
+    }
+
+    public static function legend ($legend){
+        $str = "<legend>$legend</legend>\n";
+        self::$formStr.= $str;
+        return $str;
+    }
+
+    public static function formEnd (){
+        $str = "</fieldset></form>\n";
+        self::$formStr.= $str;
+        return $str;
+    }
+
+    public static function label ($label_for, $label) {
+        $str = "<label for=\"$label_for\">$label</label><br />\n";
+        self::$formStr.= $str;
+        return $str;
+    }
+
+    public static function setValue ($name, $value){
+        if (isset(self::$values[$name])){
+            return self::$values[$name];
+        } else {
+            return '';
+        }
+
+    }
+
+    public static function text ($name, $value = '', $extra = array()){
+        if (!isset($extra['size'])){
+            $extra['size'] = HTML_FORM_TEXT_SIZE;
+        }
+
+        $value = self::setValue($name, $value);
+        $extra = self::parseExtra($extra);
+        $str = "<input type=\"text\" name=\"$name\" $extra value=\"$value\" />\n";
+        self::$formStr.= $str;
+        return $str;
+    }
+
+    public static function textarea ($name, $value = '', $extra = array()){
+        if (!isset($extra['rows'])){
+            $extra['rows'] = HTML_FORM_TEXTAREA_HT;
+        }
+
+        if (!isset($extra['cols'])){
+            $extra['cols'] = HTML_FORM_TEXTAREA_WT;
+        }
+
+        $value = self::setValue($name, $value);
+        $extra = self::parseExtra($extra);
+        $str =  "<textarea name=\"$name\" $extra>$value</textarea><br />\n";
+        self::$formStr.= $str;
+        return $str;
+    }
+
+    public static function submit ($name, $value, $extra = array ()) {
+        $extra = self::parseExtra($extra);
+
+        $str =  "<input type=\"submit\" $extra name=\"$name\" value=\"$value\" /><br />";
+        self::$formStr.= $str ;
+        return $str;
+    }
+
+    public static function parseExtra ($extra) {
+        $str = '';
+        foreach ($extra as $key => $val){
+            $str.= " $key = \"$val\" ";
+        }
+        return $str;
+    }
+
+    public static function deleteForm (){
+        
+    }
+}
