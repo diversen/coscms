9d8495e0e85c3add230f0f4f2ff2e0cdba226a03
diversen <dennis.iversen@gmail.com>
2011-04-16 10:07:03 +0200

    more simple get_module_ini function

--- lib/common.php
+++ lib/common.php
@@ -470,11 +470,6 @@ function get_module_ini($value){
         return null;
     }
 
-    /*
-    if (register::$vars['coscms_main']['module'][$value] == '0'){
-        return null;
-    }*/
-    
     return register::$vars['coscms_main']['module'][$value];
     
 }
