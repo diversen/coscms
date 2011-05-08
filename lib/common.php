5e8773743c1180d4b74ba7213a8435329a7da1ac
diversen <dennis.iversen@gmail.com>
2011-05-08 09:59:26 +0200

    accept utf8 chars in sanitize url.

--- lib/common.php
+++ lib/common.php
@@ -968,7 +968,7 @@ function cos_sanitize_url($string, $force_lowercase = true, $remove_special = fa
                    "â€”", "â€“", ",", "<", ".", ">", "/", "?");
     $clean = trim(str_replace($strip, "", strip_tags($string)));
     $clean = preg_replace('/\s+/', "-", $clean);
-    $clean = ($remove_special) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+    //$clean = ($remove_special) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
     return ($force_lowercase) ?
         (function_exists('mb_strtolower')) ?
             mb_strtolower($clean, 'UTF-8') :
