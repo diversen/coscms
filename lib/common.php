97f67deb7707df5c6c2f9c9dc737aa56022d094d
diversen <dennis.iversen@gmail.com>
2011-05-27 13:15:46 +0200

    ignore utf8 part of sanitize - use urlencode.

--- lib/common.php
+++ lib/common.php
@@ -974,14 +974,17 @@ function cos_sanitize_url($string, $force_lowercase = true, $remove_special = fa
     $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
                    "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
                    "â€”", "â€“", ",", "<", ".", ">", "/", "?");
-    $clean = trim(str_replace($strip, "", strip_tags($string)));
-    $clean = preg_replace('/\s+/', "-", $clean);
+    return $clean = trim(str_replace($strip, "", strip_tags($string)));
+    //$clean = preg_replace('/\s+/', "-", $clean);
     //$clean = ($remove_special) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+    /*
     return ($force_lowercase) ?
         (function_exists('mb_strtolower')) ?
             mb_strtolower($clean, 'UTF-8') :
             strtolower($clean) :
         $clean;
+     * 
+     */
 }
 
 
