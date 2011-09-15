2bfba7fc09c7589672803b92fbbe13487df01299
diversen <dennis.iversen@gmail.com>
2011-09-15 10:38:41 +0200

    use mb in sanitize_url in order to use utf8

--- lib/common.php
+++ lib/common.php
@@ -1118,16 +1118,16 @@ function cos_sanitize_url($string, $force_lowercase = true, $remove_special = fa
                    "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
                    "â€”", "â€“", "<", ">", "/", "?");
     return $clean = trim(str_replace($strip, "", strip_tags($string)));
-    //$clean = preg_replace('/\s+/', "-", $clean);
-    //$clean = ($remove_special) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
-    /*
+    $clean = preg_replace('/\s+/', "-", $clean);
+    $clean = ($remove_special) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+    
     return ($force_lowercase) ?
         (function_exists('mb_strtolower')) ?
             mb_strtolower($clean, 'UTF-8') :
             strtolower($clean) :
         $clean;
-     * 
-     */
+     
+     
 }
 
 /**
