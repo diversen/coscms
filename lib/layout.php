f2012d89391b164247ff45fd422683ea12cc673c
Dennis Iversen <dennis.iversen@gmail.com>
2011-10-24 21:48:57 +0200

    add som documentation.

--- lib/layout.php
+++ lib/layout.php
@@ -1,10 +1,9 @@
 <?php
 
-
-
 /**
  * File contains contains class for creating layout, which means menus and
- * blocks
+ * blocks. It operates closely with template. It loads template. In normal
+ * loading mode it is almost only used in head.
  *
  * @package    coslib
  */
@@ -15,50 +14,34 @@
  *
  * @package    coslib
  */
-
-
-class layout extends db {
+class layout {
 
     /**
      *
-     * @var     object  uri object
-     */
-    public $uri;
-
-    /**
-     *
-     * @var     array   menu for holding different menus (main / module / sub)
+     * @var array $menu for holding different menus (main / module / sub)
      */
     public static $menu = array();
 
     /**
-     * @var     string  holds if user is user, admin or super or null .)
+     * var holding all blocks content. 
+     * @var array $blocksContent holds all blocks being loaded.  
      */
-    //public static $user = null;
-    public static $blocks_sec;
-
-    public static $breadcrumbs = array();
-
-    public static $options = array();
-
     public static $blocksContent = array();
-    
-    public static $module = null;
-
 
     /**
-     * constructer method where we init our uri object and checks users
+     * constructer method and checks users
      * credentials (admin / user) thus generating
-     * accurate menus for each group.
+     * accurate menus for each group. Loading of template
      */
     function __construct(){
-
-        $this->uri = uri::getInstance();
-
-        // include template
+        
+        // check is a admin template is being used. 
         if (session::isAdmin() && isset(register::$vars['coscms_main']['admin_template'])){
             register::$vars['coscms_main']['template'] = register::$vars['coscms_main']['admin_template'];
         }
+        
+        // load template. This is done before parsing the modules. Then the 
+        // modules still can effect the template. Set header, css, js etc. 
         $template_path = 
             _COS_PATH .
             "/htdocs/templates/" .
@@ -67,57 +50,72 @@ class layout extends db {
         include_once $template_path . "/common.inc";
         include_once $template_path . "/template.inc";
     }
+    
     /**
      * method for loading menus. All Main menu entries is generated from
      * database, while all module or submodule menus are generated from
      * files (menu.inc).
-     *
+     * 
+     * XXX: The translation of the menu item is loaded. We should change this
+     * so only the untranslated string is loaded. Will make it easier to 
+     * make the site compleate multi lingual
      */
     public function loadMenus(){
-
-        $num = $this->uri->numFragments();
+        $num = uri::getInstance()->numFragments();
         $db = new db();
 
         // always a module menu in web mode
-
         self::$menu['main'] = $db->selectQuery("SELECT * FROM `menus` WHERE  (`parent` IS NULL OR `parent` = '0') AND `admin_only` IS NULL ORDER BY `weight` ASC");
 
+        // admin item are special
         if (session::isAdmin()){
             self::$menu['admin'] = $db->selectQuery("SELECT * FROM `menus` WHERE `admin_only` IS NOT NULL ORDER BY `weight` ASC");
-
         }
 
         // if status is set we don't load module menus. Must be 404 or 403.
-        // we then return empty array. module loader will know what to do.
-
+        // we then return empty array. module loader will know what to do when
+        // including correct error pages. No menus from normal main module 
+        // should then be loaded. 
         if (!empty(moduleLoader::$status)){
             return array();
         }
 
-        // get base menu from file
-
-        self::$module = $module = $this->uri->fragment(0);
+        // get base menu from file. Base menu is always loaded if found.
+        // we decide this from num fragments in uri. 
+        $module = uri::getInstance()->fragment(0);
         if ($num >= 2){
-            
             $menu = self::getBaseModuleMenu($module);
             self::$menu['module'] = $menu;
-
         }
 
+        // with num fragments larger then two we know there may be a sub module
         // parse sub level module menu if a such exists
         if ($num > 2){
-            $sub = $this->uri->fragment(0). '/' . $this->uri->fragment(1);
+            $sub = uri::getInstance()->fragment(0). '/' . 
+                   uri::getInstance()->fragment(1);
             self::$menu['sub'] = self::getSubMenu($sub);
         }
         
-        $parent = moduleLoader::getParentModule(self::$module);
+        // check if module being loaded is a child module to a parent module
+        $parent = moduleLoader::getParentModule($module);
         if (isset($parent)) {
-            self::$menu['sub'] = self::getMenuFromFile(self::$module);
+            self::$menu['sub'] = self::getMenuFromFile($module);
         }
     }
 
-    
-    function initBlocks () {
+    /**
+     * init blocks and parse blocks. 
+     * blocks which should be inited are set in main config/config.ini
+     * and looks like this: 
+     * 
+     * blocks_all = 'blocks,blocks_sec,blocks_top'
+     * 
+     * Above ini setting means that we use three block sections called:
+     *     blocks, blocks_sec and blocks_top. 
+     * These names we then can use in our template, and display the blocks 
+     * using these names. 
+     */
+    public static function initBlocks () {
         $blocks = get_main_ini('blocks_all');
         if (!isset($blocks)) return;
         //print_r($blocks); die;
@@ -128,11 +126,18 @@ class layout extends db {
         }        
     }
     
-    function getBlock ($block) {
+    /**
+     * return a whole block which can be used in templates. 
+     * e.g. blocks_top will return all blocks in section blocks_top
+     * @param type $block
+     * @return type 
+     */
+    public static function getBlock ($block) {
         return self::$blocksContent[$block];
     }
+    
     /**
-     *
+     * method for getting child menus to a module. 
      * @param  string   $module
      * @return array    children menus items
      */
@@ -146,9 +151,9 @@ class layout extends db {
     }
 
     /**
-     *
-     * @param  string   module e.g. content or content/article
-     * @return array    menu as array
+     * reads a module menu from a file. 
+     * @param  string   $module e.g. content or content/article
+     * @return array    $menu as array
      */
     public static function getMenuFromFile ($module){
         $module_menu =
@@ -171,7 +176,7 @@ class layout extends db {
     }
 
     /**
-     *
+     * gets the base modules menu. 
      * @param   string  module name
      * @return  array   array with top level module menu
      */
@@ -196,6 +201,15 @@ class layout extends db {
         return $module_menu;
     }
     
+    /**
+     * Attach a db module config menu item to a module menu. See module dbconfig
+     * This can be used for auto generating config settings for your module
+     * The url for a dbconfig setting is always $module/config/index
+     * 
+     * @param array $module_menu
+     * @param type $module
+     * @return array $module_menu now with the config menu item
+     */
     public static function setDbConfigMenuItem($module_menu, $module) {
         $config_menu_item = array (
             'url' => "/$module/config/index",
@@ -214,9 +228,9 @@ class layout extends db {
     }
 
     /**
-     *
-     * @param  string  name of sub menu to fetch $sub
-     * @return array with $_SUB_MODULE_MENU
+     * gets a module menu from file. 
+     * @param  string  $module the name of the module to get menu from
+     * @return array $module_menu the module menu. 
      */
     public static function getSubMenu ($sub) {
         return (self::getMenuFromFile($sub));
@@ -226,8 +240,12 @@ class layout extends db {
 
     /**
      * function for parsing Admin menu list.
-     * Admin menu is the menu holding all info about modules in database.
-     * Therefore it is also some sort of top level module menu.
+     * Admin menu is the menu holding all info which is only accessed
+     * by super or admin. 
+     * 
+     * Therefore it is also some sort of top level admin menu.
+     * @param array $options
+     * @return string $admin_menu as a str li ... li
      */
     public static function parseAdminMenuList ($options = array()){
 
@@ -244,6 +262,7 @@ class layout extends db {
         } else {
             $menu = self::$menu['admin'];
         }
+        
         $str = $css = '';
         foreach($menu as $k => $v){
             if ( !empty($v['auth'])){
@@ -273,9 +292,8 @@ class layout extends db {
     }
 
     /**
-     * function for parsing Admin menu list.
-     * Admin menu is the menu holding all info about modules in database.
-     * Therefore it is also some sort of top level module menu.
+     * function for parsing main menu list
+     * @return string   the main menu. as <li>item</li> etc 
      */
     public static function parseMainMenuList (){
         $menu = array();
@@ -307,7 +325,7 @@ class layout extends db {
      * A module menu is a menu connected to a main menu item.
      *
      * @param   array   menu to parse
-     * @return  string  containing menu in html form ul li
+     * @return  string  containing menu in html form <li> ... </li>
      */
     public static function parseModuleMenu($menu, $type){
 
@@ -339,10 +357,18 @@ class layout extends db {
         return $str;
     }
     
-    static function setModuleMenuExtra($items) {
+    /**
+     * method for adding extra menu items. 
+     * @param array $items menu items to add.  
+     */
+    public static function setModuleMenuExtra($items) {
         self::$menu['extra'] = $items;
     }
     
+    /**
+     *
+     * @return type 
+     */
     static function parseModuleMenuExtra () {
         $str = '';              
         $num_items = $ex = count(self::$menu['extra']);
@@ -356,7 +382,6 @@ class layout extends db {
             $str .= $v;
             $str.= "</li>\n";
         }
-        
         return "<ul>\n$str</ul>\n";
     }
 
