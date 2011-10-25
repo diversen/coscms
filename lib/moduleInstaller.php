3e8b6c86c681f589cb0127111956fdf5091a248a
Dennis Iversen <dennis.iversen@gmail.com>
2011-10-25 19:27:11 +0200

    simpler form for loading ini settings.

--- lib/moduleInstaller.php
+++ lib/moduleInstaller.php
@@ -90,12 +90,13 @@ class moduleInstaller extends db {
         if (!file_exists($ini_file)){
             if (file_exists($ini_file_dist)){
                 copy ($ini_file_dist, $ini_file);
-                register::$vars['coscms_main']['module'] = parse_ini_file($ini_file);
+                //register::$vars['coscms_main']['module'] = parse_ini_file($ini_file);
             } 
         } else {
-            register::$vars['coscms_main']['module'] = parse_ini_file($ini_file);
+            // register::$vars['coscms_main']['module'] = parse_ini_file($ini_file);
         }
-
+        
+        moduleLoader::setModuleIniSettings($options['Module']);
         if (file_exists($module_dir)){
             $install_file = "$module_dir/install.inc";
             if (!file_exists($install_file)){
