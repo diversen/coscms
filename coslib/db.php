86b331b69a70b38087a9461fac3c31a1da79a025
dennis <dennis@dennis-Rev-1-0.(none)>
2012-03-26 07:33:24 +0200

    update rb. added some methods for connecting to rb. made strings:toUTF8 static

--- coslib/db.php
+++ coslib/db.php
@@ -910,3 +910,74 @@ class dbQ extends QBuilder {
     
 }
 
+class RB {
+    public static function connect () {
+        include_once "coslib/rb.php";
+        $url = config::getMainIni('url');
+        $username = config::getMainIni('username');
+        $password = config::getMainIni('password');
+        R::setup($url, $username,$password); //mysql
+    }
+    
+    /**
+     * method for transforming an array into a bean
+     * @param object $bean
+     * @param array $ary
+     * @return object $bean 
+     */
+    public static function arrayToBean ($bean, $ary) {
+        foreach ($ary as $key => $val) {
+            $bean->{$key} = trim($val);
+        }
+        return $bean;
+    }
+    
+    /**
+     * helper function for getting a bean. It searches for an existing bean
+     * if not found it create a new bean
+     * @param string $table
+     * @param string $field
+     * @param mixed $search
+     * @return object $bean 
+     */
+    public static function getBean ($table, $field, $search) {
+        $needle = R::findOne($table," 1 AND $field  = ?", array( $search ));
+        if (empty($needle)) {
+            //log::file ("NOTICE: DISPENSE: table {$table} field {$field} search {$search}");
+            $bean = R::dispense( $table );
+        } else {
+            //log::file ("NOTICE: LOADING: table {$table} field {$field} search {$search}");
+            $bean = R::load($table, $needle->id);
+        }
+        return $bean;
+    }
+
+    /**
+     * deletes beans with transactions
+     * @param object $beans 
+     */
+    public static function deleteBeans ($beans) {
+        R::begin();
+        try{
+            R::trashAll($beans);   
+            R::commit();
+        } catch(Exception $e) {
+            R::rollback();
+        }
+    }
+
+    /**
+     * commit a bean with transactions
+     * @param object $bean 
+     */
+    public static function commitBean ($bean) {
+
+        R::begin();
+        try{
+            R::store($bean);
+            R::commit();
+        } catch(Exception $e) {
+            R::rollback();
+        }
+    }
+}
--- coslib/strings.php
+++ coslib/strings.php
@@ -105,7 +105,7 @@ class strings {
      * @param string $content
      * @return string $str
      */
-    function toUTF8($content) {
+    public static function toUTF8($content) {
         if(!mb_check_encoding($content, 'UTF-8')
             OR !($content === mb_convert_encoding(mb_convert_encoding($content, 'UTF-32', 'UTF-8' ), 'UTF-8', 'UTF-32'))) {
 
