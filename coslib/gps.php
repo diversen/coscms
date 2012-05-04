a2c5fb3b676f2d5ce0b0f660883a6a3e7f4e4b90
dennis <dennis@dennis-Rev-1-0.(none)>
2012-05-04 09:56:38 +0200

    upate showhide. add gps extractor to lib

---
+++ coslib/gps.php
@@ -0,0 +1,52 @@
+<?php
+
+// Found on stackoverflow
+// Sligtly modified
+// http://stackoverflow.com/a/4178862/464549
+function getGPS($exif, $assoc = false)
+{
+    //get the EXIF
+    //$exif = exif_read_data($fileName);
+
+    //get the Hemisphere multiplier
+    
+    $LatM = 1; $LongM = 1;
+    if($exif["GPSLatitudeRef"] == 'S')
+    {
+    $LatM = -1;
+    }
+    if($exif["GPSLongitudeRef"] == 'W')
+    {
+    $LongM = -1;
+    }
+
+    //get the GPS data
+    $gps['LatDegree']=$exif["GPSLatitude"][0];
+    $gps['LatMinute']=$exif["GPSLatitude"][1];
+    $gps['LatgSeconds']=$exif["GPSLatitude"][2];
+    $gps['LongDegree']=$exif["GPSLongitude"][0];
+    $gps['LongMinute']=$exif["GPSLongitude"][1];
+    $gps['LongSeconds']=$exif["GPSLongitude"][2];
+
+    //convert strings to numbers
+    foreach($gps as $key => $value)
+    {
+    $pos = strpos($value, '/');
+    if($pos !== false)
+    {
+        $temp = explode('/',$value);
+        $gps[$key] = $temp[0] / $temp[1];
+    }
+    }
+
+    //calculate the decimal degree
+    $result['latitude'] = $LatM * ($gps['LatDegree'] + ($gps['LatMinute'] / 60) + ($gps['LatgSeconds'] / 3600));
+    $result['longitude'] = $LongM * ($gps['LongDegree'] + ($gps['LongMinute'] / 60) + ($gps['LongSeconds'] / 3600));
+
+    if($assoc)
+    {
+    return $result;
+    }
+
+    return json_encode($result);
+}
--- htdocs/js/jquery.showhide.js
+++ htdocs/js/jquery.showhide.js
@@ -5,17 +5,19 @@
     // Create some defaults, extending them with any options that were provided
     var settings = $.extend( {
       'initial'         : 'hide',
-      'hide_class' : 'form_hide',
-      'show_class' : 'form_show',
-      'element' : '.sliding_div'
+      'element' : '.sliding_div',
+      'hide_elements' : ''
 
     }, options);
 
+    //alert(settings.hide_elements);
+    
 
     $(settings.element).hide();
     this.show();
  
     $(this).click(function(){
+        $(settings.hide_elements).hide();
         $(settings.element).slideToggle();
     });
 
