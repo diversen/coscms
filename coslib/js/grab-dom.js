08f9e0ea7be33715b8fb5def8515866ed543f224
Dennis Iversen <dennis.iversen@gmail.com>
2012-05-21 22:40:26 +0200

    update grab-dom

--- coslib/js/grab-dom.js
+++ coslib/js/grab-dom.js
@@ -1,51 +1,31 @@
 var page = new WebPage();
 var system = require('system');
-
 var url = 'http://twitter.com/#!/search/javascript';
 var timeout = 4000;  
-var test = page.injectJs('./args.js');
-console.log(test);
-//argsParser();
+
 function displayHelp () {
     console.log('Usage:');
     console.log(system.args[0] + ' \'http://twitter.com/#!/search/javascript\'');
     phantom.exit();
 }
 
-var options = {
-    'commands' : {
-        'url' : {
-            'required': true,
-            'help': 'page of url to grab'
-        },
-        'timeout' : {
-            'required': true,
-            'help': 'set timeout for loading Ajax of page to grab'
-        },
-        'help': {
-            'help': displayHelp()
-        }
-    }
-   
-}
 
-
-function argParser (options) {
+function argParser () {
     if (system.args.length === 1) {        
-        options.help.help();        
+        displayHelp();
+        phantom.exit();          
     } else {
-        console.log(system.args.length);
-        phantom.exit();
+        //console.log(system.args.length);
         url = system.args[1];
-        console.log(system.args[2]);
         if (system.args[2] != 'undefined') {
             timeout = system.args[2];
+            console.log(timeout);
         }
-        phantom.exit();        
+        //phantom.exit();        
     }
 }
 
-argParser(options);
+argParser();
 
 page.open(url, function (status) {
     if (status === 'success') {
