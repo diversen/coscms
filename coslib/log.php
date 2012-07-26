3bc0883419bb7ae0eb5071e1277b4d966fdbeff6
Dennis Iversen <dennis.iversen@gmail.com>
2012-07-26 12:38:33 +0200

    specify default log file

--- coslib/log.php
+++ coslib/log.php
@@ -71,8 +71,9 @@ function cos_error_log ($message, $write_file = null) {
     $message = strftime('%c', time()) . ": " . $message;
     $message.="\n";
     
-    $write_file = _COS_PATH . "/logs/error.log";
+    
     if ($write_file) {
+        $write_file = _COS_PATH . "/logs/error.log";
         error_log($message, 3, $write_file);
     } else {
         error_log($message);
