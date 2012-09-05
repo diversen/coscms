2cf1a18aaa9777e5fe916bbf705d297abeab0043
dennis <dennis@dennis-Rev-1-0.(none)>
2012-09-05 12:18:05 +0200

    refactor. Add common db admin methods to coscli/dbadmin.php add thin zend imap wrapper

---
+++ coslib/dbadmin.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * File contains comon methods when working in db adin mode. 
+ * @package coslib 
+ */
+
+include_once "db.php";
+
+/**
+ * dbadmin 
+ * @package
+ */
+class dbadmin extends db {
+    
+    /**
+     *  changes database we are working on
+     * @param string $database
+     */
+    function changeDB ($database = null) {
+        if (!$database) {
+            $db_curr = dbadmin::getDbInfo(); 
+            $database = $db_curr['name'];  
+        }
+        $sql = "USE `$database`";
+        $this->rawQuery($sql);
+    }
+    
+    /**
+     * gets database info from cinfuguration
+     * @return array $ary array ('name' => 'my_db, 'host' => 'localhost')
+     */
+    public static function getDbInfo() {
+        $url = parse_url(config::$vars['coscms_main']['url']);
+        $ary = explode (';', $url['path']);
+        $db = explode ("=", $ary[0]);
+        $database = array();
+        $database['name'] = $db[1];
+        $host = explode ("=", $ary[1]);
+        $database['host'] = $host[1];
+        return $database;
+    }
+}
