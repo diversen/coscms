84eca05bb4bf3a503d732db35f51493c6967c92c
dennis <dennis@dennis-laptop>
2012-10-22 12:18:13 +0200

    coscli can be called from any dir

--- coscli.sh
+++ coscli.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/php -e
 <?php
 
-define('_COS_PATH', '.');
 define('_COS_CLI', 1);
+$base_dir = dirname(__FILE__);
+define('_COS_PATH', $base_dir);
+
+$ini_path = ini_get('include_path');
+ini_set('include_path', 
+    _COS_PATH . PATH_SEPARATOR . 
+        $ini_path . PATH_SEPARATOR);
+
 include_once "coslib/shell.php";
 
 mainCli::init();
