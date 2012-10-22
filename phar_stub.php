96438db2028c787e55f1359572bbee76a65983f5
dennis <dennis@dennis-laptop>
2012-10-22 12:19:00 +0200

    clean up phar_stub

--- phar_stub.php
+++ phar_stub.php
@@ -2,10 +2,6 @@
 
 Phar::interceptFileFuncs();
 
-
-
-
-
 define('_COS_CLI', 1);
 $base_dir = dirname(__FILE__);
 define('_COS_PATH', $base_dir);
@@ -14,7 +10,6 @@ ini_set('include_path',
     _COS_PATH . PATH_SEPARATOR . 
         $ini_path . PATH_SEPARATOR);
 //ini_set('include_path', $base_dir);
-
 //Phar::mount('./backup/sql', $base_dir . '/backup/sql');
 
 include_once "coslib/head.php";
