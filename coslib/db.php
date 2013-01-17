0fe6da5047266b65ebb6c36d451d30a839f024c2
dennis <dennis@dennis-laptop>
2013-01-17 15:03:15 +0100

    add option for ignoring null values

--- coslib/db.php
+++ coslib/db.php
@@ -615,13 +615,15 @@ class db {
      * @param array $keys keys to use from request
      * @return array $ary array with post array we will use 
      */
-    public static function prepareToPostArray ($keys) {
+    public static function prepareToPostArray ($keys, $null_values = true) {
         $ary = array ();
         foreach ($keys as $val) {
             if (isset($_POST[$val])) {
                 $ary[$val] = $_POST[$val];
             } else {
-                $ary[$val] = NULL;
+                if ($null_values) {
+                    $ary[$val] = NULL;
+                }
             }
         }
         return $ary;
