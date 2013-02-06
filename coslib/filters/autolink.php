d9645fc9c8190381fb4736280ad82ac9a7bf00c2
dennis <dennis@dennis-laptop>
2013-02-06 13:13:43 +0100

    add autolink

---
+++ coslib/filters/autolink.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class autolink {
+
+    /**
+     *
+     * @param strin $text to filter
+     * @return string   $text
+     */
+    public static function filter($text){
+        
+       $text = self::autolink($text);
+       return $text;
+    }
+
+    public static function autolink ($text) {
+        
+        $text = " " . $text;
+        $text = preg_replace("#([\n ])([a-zæøå]+?)://([a-zæøå0-9\-\.,\?!%\*_\#:;~\\&$@\/=\+]+)#i", "\\1<a href=\"\\2://\\3\" target=\"_blank\">\\2://\\3</a>", $text);
+        $text = preg_replace("#([\n ])www\.([a-zæøå0-9\-]+)\.([a-zæøå0-9\-.\~]+)((?:/[a-z0-9\-\.,\?!%\*_\#:;~\\&$@\/=\+]*)?)#i", "\\1<a href=\"http://www.\\2.\\3\\4\" target=\"_blank\">www.\\2.\\3\\4</a>", $text);
+        $text = preg_replace("#([\n ])([a-z0-9\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)?[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $text);
+        $text = substr($text, 1);
+        /*
+        $text = " " . $text;
+        $text = preg_replace("#([\n ])([a-z]+?)://([a-z0-9\-\.,\?!%\*_\#:;~\\&$@\/=\+]+)#i", "\\1<a href=\"\\2://\\3\" target=\"_blank\">\\2://\\3</a>", $text);
+        $text = preg_replace("#([\n ])www\.([a-z0-9\-]+)\.([a-z0-9\-.\~]+)((?:/[a-z0-9\-\.,\?!%\*_\#:;~\\&$@\/=\+]*)?)#i", "\\1<a href=\"http://www.\\2.\\3\\4\" target=\"_blank\">www.\\2.\\3\\4</a>", $text);
+        $text = preg_replace("#([\n ])([a-z0-9\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)?[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $text);
+        $text = substr($text, 1);
+        */
+        return($text);
+    }
+
+}
