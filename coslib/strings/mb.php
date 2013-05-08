5c554b0ecaab78b8493c6f49df31f890687acdc3
dennis <dennis@dennis-laptop>
2013-05-08 11:50:47 +0200

    convert to utf8 for array

--- coslib/strings/mb.php
+++ coslib/strings/mb.php
@@ -18,13 +18,21 @@ class strings_mb {
         return strtolower($str);
     }
     
+    public static function getEncodingsAry () {
+        static $list = null;
+        if (!$list) {
+            $list = mb_list_encodings();
+        }
+        return $list;
+    }
+    
     /**
      * get encoding of a string
      * @param string $str
      * @return encoding $res e.g. ISO-8859-1 or UTF8
      */
     public static function getEncoding ($str) {
-        $list = mb_list_encodings();
+        $list = self::getEncodingsAry();
         $res = mb_detect_encoding($str, $list, true);
         return $res;
     }
@@ -39,4 +47,18 @@ class strings_mb {
         $content = mb_convert_encoding($content, $charset, 'UTF-8');
         return $content;
     } 
+    
+    /**
+     * 
+     * @param array $ary input array
+     * @return array  $ary utf8 arrau
+     */
+    public static function toUTF8Ary ($ary) {
+        $new = array ();
+        foreach ($ary as $key => $val) {
+            $new[$key] = strings::toUTF8($val);
+        }
+        return $new;
+        
+    }
 }
