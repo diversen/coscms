cf3bd66af95f4ffb7cade58b9e118d34d7e16e72
dennis <dennis@dennis-Rev-1-0.(none)>
2013-05-10 12:57:53 +0200

    add helpers for mail

---
+++ coslib/cosMail/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+class cosMail_helpers {
+    
+    public static function getDomain ($mail) {
+        $ary = explode('@', $mail);
+        if (isset($ary[1])) { 
+            return $ary[1];
+        }
+        return false;
+    }
+    
+}
