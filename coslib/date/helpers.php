76f2bcd0ef7e99ba857c67d2c8f8fdb42012289f
dennis <dennis@dennis-laptop>
2013-06-19 14:35:14 +0200

    add birthday func

--- coslib/date/helpers.php
+++ coslib/date/helpers.php
@@ -122,4 +122,19 @@ class date_helpers {
         $timestamp = mktime(0, 0, 0, $month_int);
         return strftime('%B', $timestamp);
     }
+    
+    // found on
+    // http://snippets.dzone.com/posts/show/1310
+    public static function birthday ($birthday) {
+        list($year,$month,$day) = explode("-",$birthday);
+        $year_diff  = date("Y") - $year;
+        $month_diff = date("m") - $month;
+        $day_diff   = date("d") - $day;
+        if ($month_diff < 0) { 
+            $year_diff--;
+        } elseif (($month_diff==0) && ($day_diff < 0)) { 
+            $year_diff--;
+        }
+        return $year_diff;
+    }
 }
