dfaf47be1e2d7e50bf63cf099f635e6273e6571a
dennis <dennis@dennis-laptop>
2013-10-22 11:33:28 +0200

    add class for getting apache2 version from shell

---
+++ coslib/shell/apache2.php
@@ -0,0 +1,24 @@
+<?php
+
+class shell_apache2 {
+    
+    /**
+     * 
+     * @return int $ret 0 if exists 1 if not exits
+     */
+    public static function isInstalled() {    
+        exec("which apache2", $ary, $ret);
+        return $ret;
+    }
+
+    
+    public static function getVersion() {
+        exec('apache2 -v', $ary, $ret);
+        $line = $ary[0];
+        $ary = explode(':', $line);
+        $ary[1];
+        $ary = explode('/', $ary[1]);
+        preg_match("/\d+(\.\d+)+/", $ary[1], $matches);
+        return $matches[0];  
+    }
+}
---
+++ coslib/strings/version.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * get a semantic version from a string
+ */
+class strings_version {
+    /**
+     * parses a semantic version string into a array consisting of 
+     * major, minor, minimal 2.4.6 will return: 
+     * 
+     * @param string $str e.g. 2.4.6
+     * @return array $ary array ('major' => 2, 'minor' => 4, 'minimal' => 6)
+     */
+    public static function getSemanticAry ($str) {
+        $ary = explode(".", $str);
+        $ret['major'] = $ary[0];
+        $ret['minor'] = $ary[1];
+        $ret['minimal'] = $ary[2];
+        return $ret;
+    }
+}
