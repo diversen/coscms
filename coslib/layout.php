8d4c8bb000cb72676d6487e2a00c5bb001643499
dennis <dennis@dennis-Lenovo-G500.(none)>
2013-12-14 20:10:09 +0100

    fix. Fatal on user defined blocks

--- coslib/layout.php
+++ coslib/layout.php
@@ -707,7 +707,7 @@ class layout {
             // numeric is custom block added to database
             if (is_numeric($val)) {
                 moduleloader::includeModule('block_manip');
-                $row = blocks::getOne($val); 
+                $row = block_manip::getOne($val); 
                 $row['content_block'] = moduleloader::getFilteredContent(
                     config::getModuleIni('block_manip_filters'), $row['content_block']
                 );
