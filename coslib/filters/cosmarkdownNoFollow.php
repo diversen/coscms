58a3b2b5543c6b7434cfea38f0559462aafe9c47
dennis <dennis@dennis-laptop>
2013-12-18 14:22:47 +0100

    add sisyphus.js and cosmarkdownNoFollow (markdown parser which extends michelf markdown and adds rel="nofollow" to links

---
+++ coslib/filters/cosmarkdownNoFollow.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * markdown filter. use Michelf markdown
+ * @package    filters
+ */
+
+use \Michelf\Markdown;
+
+/**
+ * markdown filter.
+ *
+ * @package    filters
+ */
+class cosmarkdownNoFollow extends \Michelf\Markdown {
+
+    public $attr = array ();
+    
+	protected function _doAnchors_reference_callback($matches) {
+		$whole_match =  $matches[1];
+		$link_text   =  $matches[2];
+		$link_id     =& $matches[3];
+
+		if ($link_id == "") {
+			# for shortcut links like [this][] or [this].
+			$link_id = $link_text;
+		}
+		
+		# lower-case and turn embedded newlines into spaces
+		$link_id = strtolower($link_id);
+		$link_id = preg_replace('{[ ]?\n}', ' ', $link_id);
+
+		if (isset($this->urls[$link_id])) {
+			$url = $this->urls[$link_id];
+			$url = $this->encodeAttribute($url);
+			
+			$result = "<a rel=\"nofollow\" href=\"$url\"";
+			if ( isset( $this->titles[$link_id] ) ) {
+				$title = $this->titles[$link_id];
+				$title = $this->encodeAttribute($title);
+				$result .=  " title=\"$title\"";
+			}
+		
+			$link_text = $this->runSpanGamut($link_text);
+			$result .= ">$link_text</a>";
+			$result = $this->hashPart($result);
+		}
+		else {
+			$result = $whole_match;
+		}
+		return $result;
+	}
+	protected function _doAnchors_inline_callback($matches) {
+		$whole_match	=  $matches[1];
+		$link_text		=  $this->runSpanGamut($matches[2]);
+		$url			=  $matches[3] == '' ? $matches[4] : $matches[3];
+		$title			=& $matches[7];
+
+		$url = $this->encodeAttribute($url);
+
+		$result = "<a rel=\"nofollow\" href=\"$url\"";
+		if (isset($title)) {
+			$title = $this->encodeAttribute($title);
+			$result .=  " title=\"$title\"";
+		}
+		
+		$link_text = $this->runSpanGamut($link_text);
+		$result .= ">$link_text</a>";
+
+		return $this->hashPart($result);
+	}
+    
+    /**
+     *
+     * @param  string     string to markdown.
+     * @return string
+     */
+    public static function filter($text){
+
+        static $md = null;
+        if (!$md){
+            $md = new cosmarkdownNoFollow();
+        }
+
+        $md->no_entities = true;
+        $md->no_markup = true;
+
+        $text = $md->transform($text);
+	return $text; 
+    }
+}
+
+class filters_cosmarkdownNoFollow extends cosmarkdownNoFollow {}
