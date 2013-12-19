173f103aed6d13bdc190bf6c5646e0ad7c1851de
dennis <dennis@dennis-laptop>
2013-12-19 15:46:36 +0100

    rm not used translation as thet are moved to system

--- lang/cosmarkdown/lang/en_GB/system.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$LANGUAGE = array ();
-$LANGUAGE['filter_cosmarkdown_help'] = '
-Content added to this field will be filtered according to the markdown syntax.
-<a href ="http://daringfireball.net/projects/markdown/syntax">This link</a>
-gives a description of the syntax. You can not use HTML directly.';
