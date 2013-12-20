2e3049700715ea10a8d19b71eaeafca5f82da751
dennis <dennis@dennis-laptop>
2013-12-20 17:05:44 +0100

    add media class to markdown images

--- coslib/filters/cosmarkdownext.php
+++ coslib/filters/cosmarkdownext.php
@@ -13,7 +13,53 @@ use \Michelf\MarkdownExtra;
  *
  * @package    filters
  */
-class cosmarkdownext {
+class cosmarkdownext extends \Michelf\MarkdownExtra {
+    
+        	protected function _doImages_reference_callback($matches) {
+		$whole_match = $matches[1];
+		$alt_text    = $matches[2];
+		$link_id     = strtolower($matches[3]);
+
+		if ($link_id == "") {
+			$link_id = strtolower($alt_text); # for shortcut links like ![this][].
+		}
+
+		$alt_text = $this->encodeAttribute($alt_text);
+		if (isset($this->urls[$link_id])) {
+			$url = $this->encodeAttribute($this->urls[$link_id]);
+			$result = "<img class=\"media_image\" src=\"$url\" alt=\"$alt_text\"";
+			if (isset($this->titles[$link_id])) {
+				$title = $this->titles[$link_id];
+				$title = $this->encodeAttribute($title);
+				$result .=  " title=\"$title\"";
+			}
+			$result .= $this->empty_element_suffix;
+			$result = $this->hashPart($result);
+		}
+		else {
+			# If there's no such link ID, leave intact:
+			$result = $whole_match;
+		}
+
+		return $result;
+	}
+	protected function _doImages_inline_callback($matches) {
+		$whole_match	= $matches[1];
+		$alt_text		= $matches[2];
+		$url			= $matches[3] == '' ? $matches[4] : $matches[3];
+		$title			=& $matches[7];
+
+		$alt_text = $this->encodeAttribute($alt_text);
+		$url = $this->encodeAttribute($url);
+		$result = "<img class=\"media_image\" src=\"$url\" alt=\"$alt_text\"";
+		if (isset($title)) {
+			$title = $this->encodeAttribute($title);
+			$result .=  " title=\"$title\""; # $title already quoted
+		}
+		$result .= $this->empty_element_suffix;
+
+		return $this->hashPart($result);
+	}
 
     /**
      *
@@ -25,7 +71,7 @@ class cosmarkdownext {
 
         static $md;
         if (!$md){
-            $md = new MarkdownExtra();
+            $md = new cosmarkdownext();
         }
 
         $md->no_entities = false;
