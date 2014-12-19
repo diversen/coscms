4ab1acfcc25fb3a8b34d635f7f2f0fbf93dbd36e
Dennis Iversen <dennis.iversen@gmail.com>
2014-12-19 20:48:52 +0100

    refactor

--- coscli.sh
+++ coscli.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env php
 <?php
 
+// is cli
 define('_COS_CLI', 1);
+
+// deinfe _COS_PATH, base path
 $base_dir = dirname(__FILE__);
 define('_COS_PATH', $base_dir);
 
+// setup based on _COS_PATH
 include_once "coslib/setup.php";
 setup::common();
 
+// include
 include_once "coslib/mainCli.php";
 
+// init and run
 mainCli::init();
 $ret = mainCli::run();
 exit($ret);
