08867accfb6c81fb1d52a525a1895b6db75ec8d6
Dennis Iversen <dennis.iversen@gmail.com>
2015-01-15 23:14:02 +0100

    auto commit

--- phar_cli.php
+++ phar_cli.php
@@ -6,6 +6,12 @@
  * see: /bin/phar_create.php
  */
 Phar::interceptFileFuncs();
+try {
+    Phar::mount('config/config.ini', '../config/config.ini');
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+} 
 
 define('_COS_CLI', 1);
 $base_dir = dirname(__FILE__);
