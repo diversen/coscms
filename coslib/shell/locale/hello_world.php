0011402b51da27eb1b418cfdb4f21daa0330329d
Dennis Iversen <dennis.iversen@gmail.com>
2015-01-16 00:36:10 +0100

    refactor

--- coslib/shell/locale/hello_world.php
+++ coslib/shell/locale/hello_world.php
@@ -21,24 +21,24 @@ function hello_world_something ($params) {
     echo $params['sentence'] . "\n";
 }
 
-mainCli::setCommand('hello_world', array(
+self::setCommand('hello_world', array(
     'description' => 'Command saying hello world.',
 ));
 
-mainCli::setOption('hello_world_output', array(
+self::setOption('hello_world_output', array(
     'long_name'   => '--say-it',
     'description' => 'Set this flag in order to really say hello world',
     'action'      => 'StoreTrue'
 ));
 
-mainCli::setOption('hello_world_something', array(
+self::setOption('hello_world_something', array(
     'long_name'   => '--say-something',
     'short_name' => '-s',
     'description' => 'Set this flag in order to say something else',
     'action'      => 'StoreTrue'
 ));
 
-mainCli::setArgument(
+self::setArgument(
     'sentence',
     array('description'=> 'Say something else',
         'optional' => true,
