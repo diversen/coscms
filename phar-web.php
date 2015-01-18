a46f177981e74d9291b6e8c2084305f54e943ed1
Dennis Iversen <dennis.iversen@gmail.com>
2015-01-18 19:04:49 +0100

    auto commit

---
+++ phar-web.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * router for self-contained phar-archive
+ * used with php built-in router. 
+ * TODO: Should work with sqlite
+ */
+Phar::interceptFileFuncs();
+try {
+    Phar::mount('config/config.ini', 'config/config.ini');
+    Phar::mount('sqlite/database.sql', 'sqlite/database.sql');
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+} 
+
+if (php_sapi_name() == 'cli-server') {
+    $info = parse_url($_SERVER['REQUEST_URI']);
+    if (file_exists( "./$info[path]") && $info['path'] != '/') {
+        return false;
+    } else {
+        include_once "index.php";
+        return true;
+    }
+}
+
+__HALT_COMPILER();
+ 
