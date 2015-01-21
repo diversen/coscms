a03da195d248341ffb7801e44ffcb9c3a5f1be0e
Dennis Iversen <dennis.iversen@gmail.com>
2015-01-21 14:30:42 +0100

    ouput static files in phar archive

--- phar-web.php
+++ phar-web.php
@@ -5,6 +5,8 @@
  * used with php built-in router. 
  */
 
+
+
 try {
 
     // mount config and database.sq outside phar archive
@@ -27,15 +29,54 @@ try {
     Phar::mount('sqlite/database.sql', '.database.sql');
 } 
 
+    function getMime($path) {
+        $result = false;
+        if (is_file($path) === true) {
+            if (function_exists('finfo_open') === true) {
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                if (is_resource($finfo) === true) {
+                    $result = finfo_file($finfo, $path);
+                }
+                finfo_close($finfo);
+            } else if (function_exists('mime_content_type') === true) {
+                $result = preg_replace('~^(.+);.*$~', '$1', mime_content_type($path));
+            } else if (function_exists('exif_imagetype') === true) {
+                $result = image_type_to_mime_type(exif_imagetype($path));
+            }
+        }
+        return $result;
+    }
+
 // If cli-server
 // This is only meant for PHP built-in server
+//Phar::mungServer(array('REQUEST_URI'));
+//Phar::webPhar(null, 'index.php');
+
 if (php_sapi_name() == 'cli-server') {
     $info = parse_url($_SERVER['REQUEST_URI']);
+    $file = $info['path'];
     if (file_exists( "./$info[path]") && $info['path'] != '/') {
-        return false;
+        
+        $full = __DIR__ . "$file"; 
+        if (!file_exists($full) OR is_dir($full) ) {
+            echo "Is dir. Or does not exists";
+            return false;
+        }
+        
+        
+        $mime = getMime($full);
+        
+        if ($mime) {
+            if ($mime == 'text/x-php') {
+                return false;
+            }
+            header("Content-Type: $mime");
+            readfile($full);
+        }
+        //return false;
     } else {
         include_once "index.php";
-        return true;
+        //return true;
     }
 }
 
