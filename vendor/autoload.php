d2a6ef628792772a29c007f5a615d19fe80df007
Dennis Iversen <dennis.iversen@gmail.com>
2015-03-12 12:58:19 +0100

    up vendor

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1f4640d3766805d0518233ce62ade5ee::getLoader();
+return ComposerAutoloaderIniteb69e33af9f3bc770128d68bcdbe90fa::getLoader();
