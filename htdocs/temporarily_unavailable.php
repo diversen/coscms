d0af06006ce8f03a20e8c7893363114a553bc350
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-02 17:11:40 +0200

    refactor

--- htdocs/temporarily_unavailable.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>This page is temporarily unavailable</title>
-    </head>
-    <body>
-        <h1>This page is temporarily unavailable</h1>
-        <p>Try again in 5 minutes</p>
-    </body>
-</html>
--- htdocs/whoami.php
+++ htdocs/whoami.php
@@ -1,4 +1,5 @@
 <?php
 
 // whoami. Determine which user the apache2 is running as
+// e.g. www-data
 echo getenv('APACHE_RUN_USER');
