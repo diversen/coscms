ec75d48887db30e8b2a859ce910bb55d75f4fe67
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-26 21:23:03 +0200

    up cli

--- coscli.sh
+++ coscli.sh
@@ -4,14 +4,11 @@
 include_once "vendor/autoload.php";
 use diversen\conf;
 use diversen\cli;
+use diversen\cli\main as mainCli;
 
 $path = dirname(__FILE__);
 conf::setMainIni('base_path', $path); 
 
-// commands are called with mainCli. 
-class mainCli extends cli{}
-
-// init and run
 mainCli::init();
 $ret = mainCli::run();
 exit($ret);
