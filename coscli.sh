87c8e5ab949e9e7b5cb807f8ec226dac5c5a7a9e
Dennis Iversen <dennis.iversen@gmail.com>
2016-12-29 01:05:44 +0100

    load module commands

--- coscli.sh
+++ coscli.sh
@@ -11,16 +11,24 @@ use diversen\lang;
 use diversen\log;
 use diversen\minimalCli;
 use diversen\db\connect;
+use diversen\moduleloader;
+use diversen\db;
+use diversen\db\q;
+use diversen\db\admin;
+use diversen\cli\common;
+
+$m = new modules();
+$m->autoloadRegister();
 
 $path = dirname(__FILE__);
 conf::setMainIni('base_path', $path);
 
 bootCli();
 
-$res = dbConn();
+
 
 $cli = new minimalCli();
-$cli->header = 'Gittobook Commandline Tool';
+$cli->header = 'CosCMS Commandline Tool';
 
 // Add commands
 $commands = [];
@@ -43,6 +51,17 @@ $commands['profile'] =          new \diversen\commands\profileCommand();
 $commands['prompt-install'] =   new \diversen\commands\promptInstall();
 $commands['useradd'] =          new \diversen\commands\useradd();
 $commands['upgrade'] =          new \diversen\commands\upgrade();
+
+
+
+$res = dbConn();
+if ($res) {
+    // echo "OK";
+    $module_commands = getModuleCommands();
+    // print_r($module_commands);die;
+    $commands = array_merge($commands, $module_commands);
+}
+
 
 $cli->commands = $commands;
 $cli->runMain();
@@ -110,4 +129,81 @@ function dbConn() {
     if ($ret == 'NO_DB_CONN') {
         return false;
     }
+    return true;
+}
+
+function tablesExists () {
+            $db = new db();
+        $ret = $db->connect(array('dont_die' => 1));
+        if ($ret == 'NO_DB_CONN'){
+            return false;
+        }
+
+        $info = admin::getDbInfo(conf::getMainIni('url'));
+        if (!$info) {
+            common::echoMessage('No databse url in config.ini');
+        }
+        if ($info['scheme'] == 'mysql' || $info['scheme'] == 'mysqli') {
+            $rows = $db->selectQuery("SHOW TABLES");
+            if (empty($rows)){
+                return false;
+            }
+            return true;
+        }
+
+        if ($info['scheme'] == 'sqlite')  {
+            $sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='modules'";
+            $rows = $db->selectQuery($sql);
+
+            if (empty($rows)){
+                return false;
+            }
+            return true;
+        }
+
+}
+
+function getModuleCommands (){
+
+    if (!tablesExists()) {
+        common::echoMessage('No tables exists. We can not load modules in modules/ dir');
+        return;
+    }
+
+    $ml = new moduleloader();
+
+    // select all db settings and merge them with ini file settings
+    $db_settings = [];
+    if (admin::tableExists('settings')) {
+        $db_settings = q::select('settings')->filter('id =', 1)->fetchSingle();
+    }
+    // merge db settings with config/config.ini settings
+    // db settings override ini file settings
+    conf::$vars['coscms_main'] = array_merge(conf::$vars['coscms_main'], $db_settings);
+
+    $commands = [];
+    $modules = moduleloader::getAllModules();
+    
+    foreach ($modules as $val) {
+        // moduleloader::setModuleIniSettings($val['module_name']);
+        $path = conf::pathModules() . "/$val[module_name]/command.php";
+        if (file_exists($path)) {
+
+            $class = '\modules\\' . "$val[module_name]\command";
+            $obj = new $class();
+            // die;
+            $help = $obj->getHelp();
+            $name = $help['name'];
+            
+            
+            $commands[$name] = $obj;
+        }
+    }
+
+    // Override any setting with configdb setting if module exists
+    if (moduleloader::moduleExists('configdb')) {
+        $c = new \modules\configdb\module();
+        $c->overrideAll();
+    }
+    return $commands;
 }
